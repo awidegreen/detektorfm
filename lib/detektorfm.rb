1cd07d2e605a1459b38eb7c850ba6125db5fa1f8
Armin Widegreen <armin.widegreen@gmail.com>
2013-09-25 23:43:37 +0200

    Improves detektorfm lib
    Stream class has been introduced to get a clear handling of musik and
    wort stream.
    detektorfm lib is not able to handling more then 100 entries, website
    will be scraped multiple times (limit/100).
    
    Binary has be updated to support new API. Further, by default no stream
    will be queried, compared to previous fetch both in case no '-m' and no
    '-w' were given.

--- lib/detektorfm.rb
+++ lib/detektorfm.rb
@@ -4,38 +4,75 @@ require 'nokogiri'
 require 'open-uri'
 
 module DetektorFm
-  PLAYLIST_URL = "http://detektor.fm/playlisten/"
+  def self.most_played_artist_overall
+    Stream.new.most_played_artist
+  end
 
-  class Request
+  class Stream
+    @@PLAYLIST_URL = "http://detektor.fm/playlisten"
 
-    def initialize
-      @html_doc = Nokogiri::HTML(open(PLAYLIST_URL))
-    end
-
-    def playlist(streams=[], limit = 100)
-      timetable = Hash.new
-      @html_doc.xpath('//table[@class="timetable"]/tbody/tr').each do |row|
-        time = row.xpath('td[@class="time"]/text()').to_s.strip
-        stream = row.xpath('td[@class="stream"]/a/text()').to_s.strip
-        artist = row.xpath('td[@class="artist"]/a/text()').to_s.strip
-        title = row.xpath('td[@class="title"]/text()').to_s.strip
-
-        timetable[stream] = Array.new if timetable[stream].nil?
-        timetable[stream] << {:time => time, :artist => artist, :title => title}
+    def played(limit = 100)
+      timetable = []
+      # only 100 are show per page, so iterate over page in case 
+      # limit >100 requested
+      (limit/101+1).times do |i|
+        url = "#{get_base_url}/P#{i*100}"
+        html_doc = Nokogiri::HTML(open(url))
+        parse_htmldoc_played(html_doc) do |time, artist, title|
+          timetable << {:time => time, :artist => artist, :title => title}
+        end
       end
 
-      timetable.each do |key,entries|
-        next unless streams.include?(key)
-        yield key, entries[0,limit]
+      timetable[0,limit].each do |e|
+          yield e[:time], e[:artist], e[:title]
       end
     end
 
-    def most_played_artist
+    def most_played_artist 
       artists = []
-      @html_doc.xpath('//ul[@class="tagcloud"]/li/a/text()').each do |a|
+      url = "#{get_base_url}"
+      html_doc = Nokogiri::HTML(open(url))
+      html_doc.xpath('//ul[@class="tagcloud"]/li/a/text()').each do |a|
         artists << a.to_s.strip
       end
       return artists
     end
+
+    def get_base_url
+      "#{@@PLAYLIST_URL}"
+    end
+
+    private
+    def parse_htmldoc_played(html_doc) 
+      html_doc.xpath('//table[@class="timetable"]/tbody/tr').each do |row|
+        time = row.xpath('td[@class="time"]/text()').to_s.strip
+        #stream = row.xpath('td[@class="stream"]/a/text()').to_s.strip
+        artist = row.xpath('td[@class="artist"]/a/text()').to_s.strip
+        title = row.xpath('td[@class="title"]/text()').to_s.strip
+
+        yield time, artist, title
+      end
+    end
   end
+
+  class MusikStream < Stream
+    def get_base_url
+      "#{@@PLAYLIST_URL}/musik"
+    end
+  
+    def get_name 
+      "MusikStream"
+    end
+  end
+
+  class WortStream < Stream
+    def get_base_url
+      "#{@@PLAYLIST_URL}/wort"
+    end
+
+    def get_name 
+      "WortStream"
+    end
+  end
+
 end
